e9986892eaaaf9b51bb1f3b29e5e59f3aa566489
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-12-07 19:01:31 +0900

    Update hw7.c

--- hw7/hw7.c
+++ hw7/hw7.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <ctype.h>  // Added for isspace function
-#include <conio.h>  // Added for kbhit and getch functions
 
 #include "Console.h"
 
@@ -17,7 +15,9 @@
 #define MAX_LEN 256
 #define ESC 27
 
-typedef struct {//±€¿⁄¿« ±À¿˚¿ª «•«ˆ
+// Í∏ÄÏûêÏùò Í∂§Ï†ÅÏùÑ ÌëúÌòÑ
+typedef struct 
+{
     char c;
     int sx, sy;     // starting coordinate
     int ex, ey;     // ending coordinate
@@ -25,7 +25,8 @@ typedef struct {//±€¿⁄¿« ±À¿˚¿ª «•«ˆ
     int vlen;       // vertical distance to move
 } MovingCharacter;
 
-typedef struct {
+typedef struct 
+{
     MovingCharacter* chars;         // pointer for the dynamically allocated array
     int* index;                     // pointer for the dynamically allocated array
     int len;                        // the # of characters
@@ -71,28 +72,31 @@ int main()
     CharBuffer cb = { NULL, NULL, 0 };          // initialization is essential
 
     int cont = TRUE;
-    while (cont) {
+    while (cont) 
+    {
         // create a character buffer with a text randomly chosen from message
         CreateCharBuffer(&cb, message[rand() % 10], screen_width, screen_height);
 
         // repeat for i from 0 to cb.len
-        for (int i = 0; i < cb.len; i++) {
+        for (int i = 0; i < cb.len; i++) 
+        {
             MovingCharacter* p = NULL;
 
             // set p to the address of the index[i]-th character
             p = &cb.chars[cb.index[i]];
 
             // if p->c is space, skip the remaining part of the loop body
-            if (isspace(p->c)) {
+            if (isspace(p->c))
                 continue;
-            }
+
 
             int x = 0, y = 0;
             // initialize (x, y) by the starting coordinate of p, print p->c at (x, y), and call "fflush(stdout)"
             fflush(stdout);
 
             // move the character from (p->sx, p->sy) to (p->ex, p->ey)
-            for (int t = 1; t <= p->vlen; t++) {
+            for (int t = 1; t <= p->vlen; t++) 
+            {
                 int prevx = 0, prevy = 0;
                 // save (x, y) in (prevx, prevy)
                 prevx = x;
@@ -112,10 +116,12 @@ int main()
                 // move the cursor at (1, screen_height), call "fflush(stdout)", and wait for 10 msec.
                 gotoxy(1, screen_height);
                 fflush(stdout);
-                MySleep(10);
+                Mysleep(10);
 
-                if (kbhit() && getch() == ESC) {  // the user pressed ESC
-                    cont = FALSE;           // breaks the outer loop
+                // the user pressed ESC
+                if (kbhit() && getch() == ESC) 
+                {
+                    cont = FALSE;           // breaks the outter loop
                     i = cb.len;             // breaks the middle loop
                     break;                  // breaks the inner loop
                 }
@@ -123,13 +129,15 @@ int main()
         }
 
         // if the user did not press ESC (cont is TRUE)
-        // move the cursor at (1, screen_height), call "fflush(stdout)", and wait for 3 sec
-        // erase the (screen_height / 2)-th row and call "fflush(stdout)"
-        if (cont == TRUE) {
+        //      move the cursor at (1, screen_height), call "fflush(stdout)", and wait for 3 sec
+        //      erase the (screen_height / 2)-th row and call "fflush(stdout)"
+        if (cont == TRUE) 
+        {
             gotoxy(1, screen_height);
             fflush(stdout);
             MySleep(3);
-            for (int i = 0; i < screen_width; i++) {
+            for (int i = 0; i < screen_width; i++) 
+            {
                 gotoxy(i, screen_height / 2);
                 printf(" ");
             }
@@ -146,31 +154,32 @@ int main()
     return 0;
 }
 
+// TO DO: create a character buffer in cb
 void CreateCharBuffer(CharBuffer* cb, char text[], int width, int height)
 {
     // if cb is not empty (cb->chars is not NULL), delete it by calling DeleteCharBuffer
-    if (cb->chars != NULL) {
+    if (cb->chars != NULL)
         DeleteCharBuffer(cb);
-    }
 
     // set cb->len to the length of text
     cb->len = strlen(text);
 
     // allocate cb->chars and cb->index (the array length should be cb->len)
-    cb->chars = (MovingCharacter*)malloc(sizeof(MovingCharacter) * cb->len);
-    cb->index = (int*)malloc(sizeof(int) * cb->len);
-
+    (cb->chars) = (struct CharBuffer*)malloc(sizeof(CharBuffer) * (cb->len));
+    (cb->index) = (struct CharBuffer*)malloc(sizeof(CharBuffer) * (cb->len));
     // on failure, display an error message and quit
-    if (cb->chars == NULL || cb->index == NULL) {
+    if (cb->chars == NULL || cb->index == NULL) 
+    {
         printf("Failed to allocate memory in Line %d of %s\n", __LINE__, __FILE__);
         exit(-1);
     }
 
     // initialize cb->chars
-    for (int i = 0; i < cb->len; i++) {
+    for (int i = 0; i < cb->len; i++) 
+    {
         MovingCharacter* p = &cb->chars[i];
 
-        // set p->c to text[i]
+        // set p->c to text[i]        
         p->c = text[i];
 
         // set p->sx to (width - 1) * i / (cb->len - 1) + 1
@@ -187,25 +196,25 @@ void CreateCharBuffer(CharBuffer* cb, char text[], int width, int height)
         p->ey = height / 2;
 
         // set p->vlen to the absolute distance between p->sy and p->ey
-        p->vlen = abs(p->sy - p->ey);
+        p->vlen = abs((p->sy) - (p->ey));
     }
 
-    // set cb->index[i] to i for all i in range [0, cb->len)
-    for (int i = 0; i < cb->len; i++) {
+    // set cb->index[i] to i for all i in range [0, cb->len
+    for (int i = 0; i <= (cb->len); i++)
         cb->index[i] = i;
-    }
 
     // shuffle index by calling ShuffleIndex()
     ShuffleIndex(cb);
 }
 
+// if cb was allocated (cb->chars is not NULL)
+//      deallocate cb->chars and cb->index
+//      for safety, set the two pointers to NULL
+//      reset cb->len to zero
 void DeleteCharBuffer(CharBuffer* cb)
 {
-    // if cb was allocated (cb->chars is not NULL)
-    // deallocate cb->chars and cb->index
-    // for safety, set the two pointers to NULL
-    // reset cb->len to zero
-    if (cb->chars != NULL) {
+    if (cb->chars != NULL) 
+    {
         free(cb->chars);
         free(cb->index);
 
@@ -216,20 +225,21 @@ void DeleteCharBuffer(CharBuffer* cb)
     }
 }
 
+// TO DO: shuffle cb->index using the algorith used in the previous homework
 void ShuffleIndex(CharBuffer* cb)
 {
-    // shuffle cb->index using the algorithm used in the previous homework
-    for (int i = 0; i < cb->len - 1; i++) {
+    for (int i = 0; i < cb->len - 1; i++) 
+    {
         int max_idx = i;
-        for (int j = i + 1; j < cb->len; j++) {
-            if (cb->index[j] > cb->index[max_idx]) {
+        for (int j = i + 1; j < cb->len; j++)
+            if (cb->index[j] > cb->index[max_idx])
                 max_idx = j;
-            }
-        }
-        if (max_idx != i) {
-            int temp = cb->index[i];
+
+        if (max_idx != i) 
+        {
+            struct CharBuffer temp = cb->index[i];
             cb->index[i] = cb->index[max_idx];
-            cb->index[max_idx] = temp;
+            cb->chars[max_idx] = temp;
         }
     }
 }
